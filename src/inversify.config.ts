b6b74d214a71cacae1b9c2a8820f6a05ee78d1f2
kalinom7 <kalinom7@DESKTOP-IDI4ILT.localdomain>
2026-01-06 20:15:19 +0100

    fix: add missing bindings to controllers

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -12,12 +12,18 @@ import {
 } from './domain/workoutschedule/WorkoutScheduleRepository.js';
 import { WorkoutController } from './application/controller/WorkoutController.js';
 import { Controller } from './application/controller/Controller.js';
+import { ExerciseController } from './application/controller/ExerciseController.js';
+import { WorkoutTemplateController } from './application/controller/WorkoutTemplateController.js';
+import { WorkoutScheduleController } from './application/controller/WorkoutScheduleController.js';
 
 const container = new Container({
   autobind: true,
 });
 
 container.bind(Controller).to(WorkoutController);
+container.bind(Controller).to(ExerciseController);
+container.bind(Controller).to(WorkoutTemplateController);
+container.bind(Controller).to(WorkoutScheduleController);
 container.bind(WorkoutRepository).to(InMemoWorkoutRepository).inSingletonScope();
 container.bind(WorkoutTemplateRepository).to(InMemoWorkoutTemplateRepository).inSingletonScope();
 container.bind(ExerciseRepository).to(InMemoExerciseRepository).inSingletonScope();
