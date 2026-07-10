a50bce5c75d850a51698418997de75c24cfd43e5
kalinom7 <mateusz29122004@gmail.com>
2026-07-10 17:53:12 +0200

    create MongoWorkoutScheduleRepository

--- src/domain/workoutschedule/WorkoutScheduleRepository.ts
+++ src/domain/workoutschedule/WorkoutScheduleRepository.ts
@@ -1,5 +1,8 @@
 import { injectable } from 'inversify';
 import { type WorkoutSchedule } from './model/WorkoutSchedule.js';
+import { MongoConnection } from '../../application/MongoConnection.js';
+import { Collection } from 'mongodb';
+import { UUID } from 'node:crypto';
 
 export abstract class WorkoutScheduleRepository {
   public abstract save(workoutSchedule: WorkoutSchedule): Promise<void>;
@@ -7,6 +10,33 @@ export abstract class WorkoutScheduleRepository {
   public abstract getAll(userId: string): Promise<WorkoutSchedule[]>;
   public abstract getActive(userId: string): Promise<WorkoutSchedule | null>;
   public abstract delete(workoutScheduleId: string, userId: string): Promise<void>;
+}
+
+@injectable()
+export class MongoWorkoutScheduleRepository extends WorkoutScheduleRepository {
+  constructor(private readonly mongoConnection: MongoConnection) {
+    super();
+  }
+
+  private get collection(): Collection<WorkoutSchedule> {
+    return this.mongoConnection.getDb().collection<WorkoutSchedule>('workoutSchedules');
+  }
+
+  public override async save(workoutSchedule: WorkoutSchedule): Promise<void> {
+    await this.collection.updateOne({ id: workoutSchedule.id }, { $set: { workoutSchedule } }, { upsert: true });
+  }
+  public override get(workoutScheduleId: UUID, userId: UUID): Promise<WorkoutSchedule | null> {
+    return this.collection.findOne({ id: workoutScheduleId, userId: userId }, { projection: { _id: 0 } });
+  }
+  public override async getAll(userId: UUID): Promise<WorkoutSchedule[]> {
+    return this.collection.find({ userId: userId }, { projection: { _id: 0 } }).toArray();
+  }
+  public override getActive(userId: UUID): Promise<WorkoutSchedule | null> {
+    return this.collection.findOne({ userId: userId, isActive: true }, { projection: { _id: 0 } });
+  }
+  public override async delete(workoutScheduleId: UUID, userId: UUID): Promise<void> {
+    await this.collection.deleteOne({ id: workoutScheduleId, userId: userId });
+  }
 }
 
 @injectable()
