5c0c617c921d8584cd8d69c07ec2884f11d3a6f8
kalinom7 <mateusz29122004@gmail.com>
2026-07-11 18:24:41 +0200

    remove unused workout repository code

--- src/domain/workout/WorkoutRepository.ts
+++ src/domain/workout/WorkoutRepository.ts
@@ -1,16 +1,9 @@
 import { injectable } from 'inversify';
 import { type Workout } from './model/Workout.js';
-import { UUID } from 'node:crypto';
 
 export abstract class WorkoutRepository {
   public abstract get(workoutId: string, userId: string): Promise<Workout | null>;
   public abstract getAllFinished(userId: string): Promise<Workout[] | null>;
-  public abstract findLastFinishedFromPattern(
-    userId: string,
-    workoutTemplateIds: UUID[],
-    scheduleStartDate: Date,
-  ): Promise<Workout | null>;
-
   public abstract save(workout: Workout): Promise<void>;
 }
 
@@ -25,34 +18,6 @@ export class InMemoWorkoutRepository extends WorkoutRepository {
     }
 
     return null;
-  }
-
-  /**
-   * Returns the most recently completed workout for the given user
-   * that matches any of the provided workout template IDs.
-   *
-   * Used to determine the correct order in WorkoutSchedule.
-   */
-  public async findLastFinishedFromPattern(
-    userId: UUID,
-    workoutTemplateIds: UUID[],
-    scheduleStartDate: Date,
-  ): Promise<Workout | null> {
-    let lastFinishedWorkout: Workout | null = null;
-
-    for (const workout of this.workouts.values()) {
-      if (workout.userId !== userId) continue;
-
-      if (!workout.endTime || workout.endTime < scheduleStartDate) continue;
-
-      if (workout.usedWorkoutTemplate !== null && !workoutTemplateIds.includes(workout.usedWorkoutTemplate)) continue;
-
-      if (!lastFinishedWorkout || workout.endTime > lastFinishedWorkout.endTime!) {
-        lastFinishedWorkout = workout;
-      }
-    }
-
-    return lastFinishedWorkout ?? null;
   }
 
   public async getAllFinished(userId: string): Promise<Workout[]> {
