ca37beb3b215234557d4c795653a34a336207c6e
kalinom7 <mateusz29122004@gmail.com>
2026-07-19 17:09:02 +0200

    change mongoWorkoutRepository to store workoutId in _id field

--- src/application/repository/Workout/MongoWorkoutRepository.ts
+++ src/application/repository/Workout/MongoWorkoutRepository.ts
@@ -4,21 +4,42 @@ import { Workout } from '../../../domain/workout/model/Workout.js';
 import { Collection, Db } from 'mongodb';
 import { UUID } from 'node:crypto';
 
+export type MongoWorkout = Omit<Workout, 'id'> & { _id: UUID };
 @injectable()
 export class MongoWorkoutRepository extends WorkoutRepository {
   constructor(private readonly db: Db) {
     super();
   }
-  private get collection(): Collection<Workout> {
-    return this.db.collection<Workout>('workouts');
+  private get collection(): Collection<MongoWorkout> {
+    return this.db.collection<MongoWorkout>('workouts');
   }
   public async get(workoutId: UUID, userId: UUID): Promise<Workout | null> {
-    return this.collection.findOne({ id: workoutId, userId: userId }, { projection: { _id: 0 } });
+    const mongoWorkout = await this.collection.findOne({ _id: workoutId, userId: userId });
+
+    return mongoWorkout ? this.toDomainWorkout(mongoWorkout) : null;
   }
   public async getAllFinished(userId: UUID): Promise<Workout[] | null> {
-    return this.collection.find({ userId: userId }, { projection: { _id: 0 } }).toArray();
+    const mongoWorkouts = await this.collection.find({ userId: userId }).toArray();
+    const domainWorkouts = mongoWorkouts.map((mongoWorkout) => this.toDomainWorkout(mongoWorkout));
+
+    return domainWorkouts;
   }
   public async save(workout: Workout): Promise<void> {
-    await this.collection.updateOne({ id: workout.id, userId: workout.userId }, { $set: workout }, { upsert: true });
+    const mongoWorkout = this.toMongoWorkout(workout);
+    await this.collection.updateOne(
+      { _id: mongoWorkout._id, userId: workout.userId },
+      { $set: mongoWorkout },
+      { upsert: true },
+    );
+  }
+  private toMongoWorkout(workout: Workout): MongoWorkout {
+    const { id, ...workoutData } = workout;
+
+    return { _id: id, ...workoutData };
+  }
+  private toDomainWorkout(mongoWorkout: MongoWorkout): Workout {
+    const { _id, ...workoutData } = mongoWorkout;
+
+    return { id: _id, ...workoutData };
   }
 }
--- test/domain/workout/MongoWorkoutRepository.test.ts
+++ test/domain/workout/MongoWorkoutRepository.test.ts
@@ -1,7 +1,10 @@
 import {} from '@golevelup/ts-jest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { MongoConnection } from '../../../src/application/MongoConnection';
-import { MongoWorkoutRepository } from '../../../src/application/repository/Workout/MongoWorkoutRepository';
+import {
+  type MongoWorkout,
+  MongoWorkoutRepository,
+} from '../../../src/application/repository/Workout/MongoWorkoutRepository';
 import process from 'process';
 import { type Collection } from 'mongodb';
 import { type Workout } from '../../../src/domain/workout/model/Workout';
@@ -11,7 +14,7 @@ describe('MongoWorkoutRepository', () => {
   let mongod: MongoMemoryServer;
   let mongoConnection: MongoConnection;
   let repository: MongoWorkoutRepository;
-  let collection: Collection<Workout>;
+  let collection: Collection<MongoWorkout>;
   const originalUrl = process.env['MONGO_URL'];
   const originalDb = process.env['MONGO_DATABASE'];
 
@@ -28,7 +31,7 @@ describe('MongoWorkoutRepository', () => {
     process.env['MONGO_DATABASE'] = originalDb;
   });
   beforeEach(async () => {
-    collection = mongoConnection.getDb().collection<Workout>('workouts');
+    collection = mongoConnection.getDb().collection<MongoWorkout>('workouts');
     await collection.deleteMany({});
     repository = new MongoWorkoutRepository(mongoConnection.getDb());
   });
@@ -45,66 +48,67 @@ describe('MongoWorkoutRepository', () => {
       endTime: new Date(),
       exercises: [],
     };
-
+    const { id, ...workoutData } = workout;
     //when
     await repository.save(workout);
 
     //then
-    const savedWorkout = await collection.findOne(workout, { projection: { _id: 0 } });
-    expect(savedWorkout).toEqual(workout);
+    const savedWorkout = await collection.findOne({ _id: workoutId, userId });
+    expect(savedWorkout).toEqual({ _id: workoutId, ...workoutData });
   });
   test('should get existing workout', async () => {
     //given
     const workoutId = randomUUID();
     const userId = randomUUID();
-    const workout: Workout = {
-      id: workoutId,
+    const workout: MongoWorkout = {
+      _id: workoutId,
       userId: userId,
       usedWorkoutTemplate: randomUUID(),
       startTime: new Date('2026-07-11T10:30:00'),
       endTime: null,
       exercises: [],
     };
+    const { _id, ...workoutData } = workout;
     await collection.insertOne({ ...workout });
     //when
     const returnedWorkout = await repository.get(workoutId, userId);
 
     //then
-    expect(returnedWorkout).toEqual(workout);
+    expect(returnedWorkout).toEqual({ id: workoutId, ...workoutData });
   });
   test('should get all finished workouts', async () => {
     //given
     const userId = randomUUID();
-    const workout1: Workout = {
-      id: randomUUID(),
+    const workout1: MongoWorkout = {
+      _id: randomUUID(),
       userId: userId,
       usedWorkoutTemplate: randomUUID(),
       startTime: new Date('2026-07-11T10:30:00'),
       endTime: null,
       exercises: [],
     };
-    const workout2: Workout = {
-      id: randomUUID(),
+    const workout2: MongoWorkout = {
+      _id: randomUUID(),
       userId: userId,
       usedWorkoutTemplate: randomUUID(),
       startTime: new Date('2026-07-11T10:30:00'),
       endTime: null,
       exercises: [],
     };
-    const workout3: Workout = {
-      id: randomUUID(),
+    const workout3: MongoWorkout = {
+      _id: randomUUID(),
       userId: userId,
       usedWorkoutTemplate: randomUUID(),
       startTime: new Date('2026-07-11T10:30:00'),
       endTime: null,
       exercises: [],
     };
-    const workouts: Workout[] = [workout1, workout2, workout3];
+    const workouts: MongoWorkout[] = [workout1, workout2, workout3];
     await collection.insertMany(workouts.map((workout) => ({ ...workout })));
     //when
     const returnedWorkouts = await repository.getAllFinished(userId);
 
     //then
-    expect(returnedWorkouts).toEqual(workouts);
+    expect(returnedWorkouts).toEqual(workouts.map(({ _id, ...workoutData }) => ({ id: _id, ...workoutData })));
   });
 });
