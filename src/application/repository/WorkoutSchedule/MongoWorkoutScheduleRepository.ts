ede14505b80cc6086b1f2923efd390cefa2b46da
kalinom7 <mateusz29122004@gmail.com>
2026-07-19 16:46:53 +0200

    change mongoWorkoutScheduleRepository to store id in _id field

--- src/application/repository/WorkoutSchedule/MongoWorkoutScheduleRepository.ts
+++ src/application/repository/WorkoutSchedule/MongoWorkoutScheduleRepository.ts
@@ -4,29 +4,55 @@ import { WorkoutSchedule } from '../../../domain/workoutschedule/model/WorkoutSc
 import { Collection, Db } from 'mongodb';
 import { UUID } from 'node:crypto';
 
+export type MongoWorkoutSchedule = Omit<WorkoutSchedule, 'id'> & { _id: UUID };
 @injectable()
 export class MongoWorkoutScheduleRepository extends WorkoutScheduleRepository {
   constructor(private readonly db: Db) {
     super();
   }
 
-  private get collection(): Collection<WorkoutSchedule> {
-    return this.db.collection<WorkoutSchedule>('workoutSchedules');
+  private get collection(): Collection<MongoWorkoutSchedule> {
+    return this.db.collection<MongoWorkoutSchedule>('workoutSchedules');
   }
 
-  public override async save(workoutSchedule: WorkoutSchedule): Promise<void> {
-    await this.collection.updateOne({ id: workoutSchedule.id }, { $set: workoutSchedule }, { upsert: true });
+  public async save(workoutSchedule: WorkoutSchedule): Promise<void> {
+    const mongoWorkoutSchedule = this.toMongoWorkoutSchedule(workoutSchedule);
+    await this.collection.updateOne(
+      { _id: mongoWorkoutSchedule._id },
+      { $set: mongoWorkoutSchedule },
+      { upsert: true },
+    );
   }
-  public override get(workoutScheduleId: UUID, userId: UUID): Promise<WorkoutSchedule | null> {
-    return this.collection.findOne({ id: workoutScheduleId, userId: userId }, { projection: { _id: 0 } });
+  public async get(workoutScheduleId: UUID, userId: UUID): Promise<WorkoutSchedule | null> {
+    const mongoWorkoutSchedule = await this.collection.findOne({ _id: workoutScheduleId, userId: userId });
+
+    return mongoWorkoutSchedule ? this.toDomainWorkoutSchedule(mongoWorkoutSchedule) : null;
   }
-  public override async getAll(userId: UUID): Promise<WorkoutSchedule[]> {
-    return this.collection.find({ userId: userId }, { projection: { _id: 0 } }).toArray();
+  public async getAll(userId: UUID): Promise<WorkoutSchedule[]> {
+    const mongoWorkoutSchedules = await this.collection.find({ userId: userId }).toArray();
+    const domainWorkoutSchedules = mongoWorkoutSchedules.map((mongoWorkoutSchedule) =>
+      this.toDomainWorkoutSchedule(mongoWorkoutSchedule),
+    );
+
+    return domainWorkoutSchedules;
   }
-  public override getActive(userId: UUID): Promise<WorkoutSchedule | null> {
-    return this.collection.findOne({ userId: userId, isActive: true }, { projection: { _id: 0 } });
+  public async getActive(userId: UUID): Promise<WorkoutSchedule | null> {
+    const mongoWorkoutSchedule = await this.collection.findOne({ userId: userId, isActive: true });
+
+    return mongoWorkoutSchedule ? this.toDomainWorkoutSchedule(mongoWorkoutSchedule) : null;
   }
-  public override async delete(workoutScheduleId: UUID, userId: UUID): Promise<void> {
-    await this.collection.deleteOne({ id: workoutScheduleId, userId: userId });
+  public async delete(workoutScheduleId: UUID, userId: UUID): Promise<void> {
+    await this.collection.deleteOne({ _id: workoutScheduleId, userId: userId });
+  }
+
+  private toMongoWorkoutSchedule(workoutSchedule: WorkoutSchedule): MongoWorkoutSchedule {
+    const { id, ...workoutScheduleData } = workoutSchedule;
+
+    return { _id: id, ...workoutScheduleData };
+  }
+  private toDomainWorkoutSchedule(mongoWorkoutSchedule: MongoWorkoutSchedule): WorkoutSchedule {
+    const { _id, ...workoutScheduleData } = mongoWorkoutSchedule;
+
+    return { id: _id, ...workoutScheduleData };
   }
 }
--- test/domain/workoutschedule/MongoWorkoutScheduleRepository.test.ts
+++ test/domain/workoutschedule/MongoWorkoutScheduleRepository.test.ts
@@ -2,16 +2,19 @@ import {} from '@golevelup/ts-jest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { MongoConnection } from '../../../src/application/MongoConnection';
 import process from 'process';
-import { MongoWorkoutScheduleRepository } from '../../../src/application/repository/WorkoutSchedule/MongoWorkoutScheduleRepository';
+import {
+  type MongoWorkoutSchedule,
+  MongoWorkoutScheduleRepository,
+} from '../../../src/application/repository/WorkoutSchedule/MongoWorkoutScheduleRepository';
 import { type Collection } from 'mongodb';
 import { type WorkoutSchedule } from '../../../src/domain/workoutschedule/model/WorkoutSchedule';
-import { randomUUID } from 'crypto';
+import { randomUUID } from 'node:crypto';
 
 describe('MongoWorkoutScheduleRepository', () => {
   let mongod: MongoMemoryServer;
   let mongoConnection: MongoConnection;
   let repository: MongoWorkoutScheduleRepository;
-  let collection: Collection<WorkoutSchedule>;
+  let collection: Collection<MongoWorkoutSchedule>;
   const originalUrl = process.env['MONGO_URL'];
   const originalDb = process.env['MONGO_DATABASE'];
 
@@ -28,7 +31,7 @@ describe('MongoWorkoutScheduleRepository', () => {
     await mongod.stop();
   });
   beforeEach(async () => {
-    collection = mongoConnection.getDb().collection<WorkoutSchedule>('workoutSchedules');
+    collection = mongoConnection.getDb().collection<MongoWorkoutSchedule>('workoutSchedules');
     await collection.deleteMany({});
     repository = new MongoWorkoutScheduleRepository(mongoConnection.getDb());
   });
@@ -47,20 +50,21 @@ describe('MongoWorkoutScheduleRepository', () => {
       lastFinishedWorkoutDate: null,
       lastOrder: null,
     };
+    const { id, ...workoutScheduleData } = workoutSchedule;
 
     //when
     await repository.save(workoutSchedule);
 
     //then
-    const savedWorkoutSchedule = await collection.findOne({ id: workoutScheduleId }, { projection: { _id: 0 } });
-    expect(savedWorkoutSchedule).toEqual(workoutSchedule);
+    const savedWorkoutSchedule = await collection.findOne({ _id: workoutScheduleId });
+    expect(savedWorkoutSchedule).toEqual({ _id: workoutScheduleId, ...workoutScheduleData });
   });
   test('should save changes to existing workout schedule', async () => {
     //given
     const workoutScheduleId = randomUUID();
     const userId = randomUUID();
-    const workoutSchedule: WorkoutSchedule = {
-      id: workoutScheduleId,
+    const workoutSchedule: MongoWorkoutSchedule = {
+      _id: workoutScheduleId,
       userId: userId,
       name: 'test workout schedule',
       isActive: false,
@@ -79,24 +83,22 @@ describe('MongoWorkoutScheduleRepository', () => {
       lastFinishedWorkoutDate: null,
       lastOrder: null,
     };
+    const { id, ...changedWorkoutScheduleData } = changedWorkoutSchedule;
     await collection.insertOne(workoutSchedule);
 
     //when
     await repository.save(changedWorkoutSchedule);
 
     //then
-    const savedWorkoutSchedule = await collection.findOne(
-      { id: workoutScheduleId, userId: userId },
-      { projection: { _id: 0 } },
-    );
-    expect(savedWorkoutSchedule).toEqual(changedWorkoutSchedule);
+    const savedWorkoutSchedule = await collection.findOne({ _id: workoutScheduleId, userId: userId });
+    expect(savedWorkoutSchedule).toEqual({ _id: workoutScheduleId, ...changedWorkoutScheduleData });
   });
   test('should get existing schedule', async () => {
     //given
     const workoutScheduleId = randomUUID();
     const userId = randomUUID();
-    const workoutSchedule: WorkoutSchedule = {
-      id: workoutScheduleId,
+    const workoutSchedule: MongoWorkoutSchedule = {
+      _id: workoutScheduleId,
       userId: userId,
       name: 'test workout schedule',
       isActive: false,
@@ -105,20 +107,23 @@ describe('MongoWorkoutScheduleRepository', () => {
       lastFinishedWorkoutDate: null,
       lastOrder: null,
     };
+    const { _id, ...workoutScheduleData } = workoutSchedule;
     await collection.insertOne({ ...workoutSchedule });
 
     //when
     const returnedWorkoutSchedule = await repository.get(workoutScheduleId, userId);
 
     //then
-    expect(returnedWorkoutSchedule).toEqual(workoutSchedule);
+    expect(returnedWorkoutSchedule).toEqual({ id: workoutScheduleId, ...workoutScheduleData });
   });
   test('should get all user workout schedules', async () => {
     //given
-    const workoutScheduleId = randomUUID();
+    const workoutScheduleId1 = randomUUID();
+    const workoutScheduleId2 = randomUUID();
+    const workoutScheduleId3 = randomUUID();
     const userId = randomUUID();
-    const workoutSchedule1: WorkoutSchedule = {
-      id: workoutScheduleId,
+    const workoutSchedule1: MongoWorkoutSchedule = {
+      _id: workoutScheduleId1,
       userId: userId,
       name: 'test workout schedule1',
       isActive: false,
@@ -127,8 +132,8 @@ describe('MongoWorkoutScheduleRepository', () => {
       lastFinishedWorkoutDate: null,
       lastOrder: null,
     };
-    const workoutSchedule2: WorkoutSchedule = {
-      id: workoutScheduleId,
+    const workoutSchedule2: MongoWorkoutSchedule = {
+      _id: workoutScheduleId2,
       userId: userId,
       name: 'test workout schedule2',
       isActive: false,
@@ -137,8 +142,8 @@ describe('MongoWorkoutScheduleRepository', () => {
       lastFinishedWorkoutDate: null,
       lastOrder: null,
     };
-    const workoutSchedule3: WorkoutSchedule = {
-      id: workoutScheduleId,
+    const workoutSchedule3: MongoWorkoutSchedule = {
+      _id: workoutScheduleId3,
       userId: userId,
       name: 'test workout schedule3',
       isActive: false,
@@ -147,21 +152,27 @@ describe('MongoWorkoutScheduleRepository', () => {
       lastFinishedWorkoutDate: null,
       lastOrder: null,
     };
-    const schedules: WorkoutSchedule[] = [workoutSchedule1, workoutSchedule2, workoutSchedule3];
+    const schedules: MongoWorkoutSchedule[] = [workoutSchedule1, workoutSchedule2, workoutSchedule3];
 
     await collection.insertMany(schedules.map((schedule) => ({ ...schedule })));
     //when
     const returnedSchedules = await repository.getAll(userId);
 
     //then
-    expect(returnedSchedules).toEqual(schedules);
+    expect(returnedSchedules).toEqual(
+      schedules.map((schedule) => {
+        const { _id, ...scheduleData } = schedule;
+
+        return { id: _id, ...scheduleData };
+      }),
+    );
   });
   test('should return active workout schedule', async () => {
     //given
     const workoutScheduleId = randomUUID();
     const userId = randomUUID();
-    const workoutSchedule: WorkoutSchedule = {
-      id: workoutScheduleId,
+    const workoutSchedule: MongoWorkoutSchedule = {
+      _id: workoutScheduleId,
       userId: userId,
       name: 'test active workout schedule',
       isActive: true,
@@ -170,19 +181,20 @@ describe('MongoWorkoutScheduleRepository', () => {
       lastFinishedWorkoutDate: null,
       lastOrder: null,
     };
+    const { _id, ...workoutScheduleData } = workoutSchedule;
     await collection.insertOne({ ...workoutSchedule });
     //when
     const returnedActiveSchedule = await repository.getActive(userId);
     //then
-    expect(returnedActiveSchedule).toEqual(workoutSchedule);
+    expect(returnedActiveSchedule).toEqual({ id: _id, ...workoutScheduleData });
   });
 
   test('should delete existing workout schedule', async () => {
     //given
     const workoutScheduleId = randomUUID();
     const userId = randomUUID();
-    const workoutSchedule: WorkoutSchedule = {
-      id: workoutScheduleId,
+    const workoutSchedule: MongoWorkoutSchedule = {
+      _id: workoutScheduleId,
       userId: userId,
       name: 'test workout schedule',
       isActive: true,
@@ -197,7 +209,7 @@ describe('MongoWorkoutScheduleRepository', () => {
     await repository.delete(workoutScheduleId, userId);
 
     //then
-    const deletedWorkoutSchedule = await collection.findOne(workoutSchedule);
+    const deletedWorkoutSchedule = await collection.findOne({ _id: workoutScheduleId });
     expect(deletedWorkoutSchedule).toBeNull();
   });
 });
